d6dc463df9181740ff0fdd187965ccc941ca37b7
ESB-codes <ekamsingh643@gmail.com>
2022-09-21 23:02:23 +0530

    commit: mark11

---
+++ app.js
@@ -0,0 +1,37 @@
+let Bday=document.querySelector("#in");
+let luckyNumber=document.querySelector("#lucky");
+let checkB=document.querySelector("#check");
+let resultEl=document.querySelector("#result");
+
+// this function removes hyphen, calculate sum by typecasting CharAt
+function calculateSum(x){
+    let dob=x.replaceAll("-","");
+    console.log(dob)
+
+    let sum=0;
+    for (let i=0; i<dob.length; i++){
+        sum+= Number(dob.charAt(i));
+    }
+            return sum
+
+}
+
+function checkIfLuckyNumber(){
+
+    let number=Bday.value;
+    let sum= calculateSum(number)
+
+    if (sum && luckyNumber.value ){
+        if(sum%luckyNumber.value===0){
+            resultEl.innerText="Lucky😇"
+        }else{
+            resultEl.innerText="not lucky😭"
+        }
+
+    }else{
+            resultEl.innerText="Pls fill both the values"
+    }
+
+}
+
+checkB.addEventListener("click", checkIfLuckyNumber)
